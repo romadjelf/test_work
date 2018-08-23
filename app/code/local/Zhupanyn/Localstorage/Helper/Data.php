eb5a7f56b5ca31f69aa9c01fc7ef60048aff1291
romadjelf <roma.dj.elf@gmail.com>
2018-08-23 11:49:10 +0300

    ТЗ9-v1, Переглянуті товари

---
+++ app/code/local/Zhupanyn/Localstorage/Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+class Zhupanyn_Localstorage_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getDomain()
+    {
+        $cookie = Mage::getModel('core/cookie');
+        return $cookie->getDomain();
+    }
+
+    public function getTime()
+    {
+        $time = Mage::getStoreConfig('catalog/recently_products/viewed_time');
+        return $time;
+    }
+}
+?>
